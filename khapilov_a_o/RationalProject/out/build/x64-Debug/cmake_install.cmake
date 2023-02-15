6add5c031a8e03bedafc13a666d2cb035240c4d1
WeCanStart <113310973+WeCanStart@users.noreply.github.com>
2023-02-15 20:03:09 +0300

    out added

--- khapilov_a_o/RationalProject/out/build/x64-Debug/cmake_install.cmake
+++ khapilov_a_o/RationalProject/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/artem/source/repos/WeCanStart/Programming/khapilov_a_o/Rational
+# Install script for directory: C:/Users/artem/source/repos/Programming/khapilov_a_o/RationalProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/artem/Source/Repos/WeCanStart/Programming/khapilov_a_o/Rational/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/artem/source/repos/Programming/khapilov_a_o/RationalProject/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,6 +32,12 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/artem/source/repos/Programming/khapilov_a_o/RationalProject/out/build/x64-Debug/rational/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -40,5 +46,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/artem/source/repos/WeCanStart/Programming/khapilov_a_o/Rational/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/artem/source/repos/Programming/khapilov_a_o/RationalProject/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
